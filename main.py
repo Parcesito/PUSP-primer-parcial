3576ff0a477ccab7a6d6b4dd69bf9ab965eaa3ee
Parcesito <diederiiik@gmail.com>
2024-03-09 11:14:15 -0500

    Actualización #5

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
-import leasable
-import shop
-import product
 import admin_options
+import person_options
+import time
+import os
+from person import Person
+from shop import Shop
+from user import User
+from datetime import date
 
-admin_options.modify_shop([])
+
+def pusp(users: list, admins: list,  shops: list[Shop]):
+    bandera = 0
+    while True:
+        print("--------------------- Inicio de sesión ---------------------\n")
+        name = input("                Usuario: ")
+        password = input("                contraseña:")
+        print("\n--------------------------------------------------------------")
+
+        for user in users:
+            if user.user == name and user.password == password:
+                bandera = 1
+                person_options.user_menu(user, shops)
+                break
+        if bandera == 0:
+            for admin in admins:
+                if admin.user == name and admin.password == password:
+                    bandera = 1
+                    admin_options.admin_menu(users, shops)
+                    break
+
+        if bandera == 0:
+            print("--------------------------------------------------------------\n")
+            print("                     Usuario no encontrado \n")
+            print("--------------------------------------------------------------\n")
+
+        print("--------------------- Inicio de sesión ---------------------\n")
+        bandera = input("¿Desea iniciar sesión nuevamente Y/N: ")
+        if bandera == "Y":
+            continue
+
+        print("--------------------------------------------------------------\n")
+        print("            ¡Muchas gracias por utilizar el programa! \n")
+        print("--------------------------------------------------------------\n")
+
+
+users = [Person(2000, "Carlos", "Álveres", date(1980, 12, 5), "user123",
+               "12345", False, 15000)]
+
+admins = [User(1000, "Jhon", "Doe", date(1980, 12, 28), "admin123",
+       "12345", True)]
+shops = []
+
+pusp(users, admins, shops)
